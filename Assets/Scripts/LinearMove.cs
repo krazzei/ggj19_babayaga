054143766c6378e819ab23fe41c98d438c0acdea
Tyler White <twigwhite@gmail.com>
2019-01-26 17:59:12 -0600

    Initial commit of baba yaga Added a couple scripts from broken, modified the PlayerMove scritp to support this game.

---
+++ Assets/Scripts/LinearMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMove : MonoBehaviour
+{
+	private Transform _transform;
+
+	public Vector3 Direction;
+
+	public float Speed;
+
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		_transform = transform;
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		_transform.Translate(Direction * Speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private Transform _transform;
+
+	public float StartSpeed;
+
+	private float _speed;
+	
+	private void Awake()
+	{
+		_transform = transform;
+		_speed = StartSpeed;
+	}
+
+	private void Update()
+	{
+		var dir = new Vector3(Input.GetAxis("Horizontal") * _speed * Time.deltaTime, Input.GetAxis("Vertical") * _speed * Time.deltaTime, 0);
+		_transform.Translate(dir);
+	}
+}
