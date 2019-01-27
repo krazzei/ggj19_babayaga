a6e45d959e4f903c6f55e5854d7405550cb33c12
Tyler White <twigwhite@gmail.com>
2019-01-27 12:44:00 -0600

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UI = UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject ButtonPanel;
+
+    public GameObject CreditsPanel;
+
+    public EventSystem _eventSystem;
+
+    public GameObject CreditsBackButton;
+    public GameObject MainMenuPlayButton;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+    public void ShowButtons()
+    {
+        ButtonPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        
+        _eventSystem.SetSelectedGameObject(MainMenuPlayButton);
+    }
+
+    public void ShowCredits()
+    {
+        ButtonPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+
+        _eventSystem.SetSelectedGameObject(CreditsBackButton);
+    }
+}
